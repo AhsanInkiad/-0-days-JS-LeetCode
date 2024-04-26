76e64d1ec6d409d64c51812d041c9555a81356b7
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2024-04-26 17:31:49 +0600

    problem: 2721. Execute Asynchronous Functions in Parallel

---
+++ problem19.js
@@ -0,0 +1,12 @@
+// 2721. Execute Asynchronous Functions in Parallel
+
+var promiseAll = function(functions) {
+    return new Promise((res, rej)=>{
+        
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
